d5e0f73a4d738b52a29cffe7bd2e0c3cb0d0ace1
avrilyql <avrilyql2027@student.cis.edu.hk>
2024-10-01 19:06:59 +0800

    finished IDE quiz

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str) {
+        int numVowels = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                numVowels++;
+            }
+        }
+        return numVowels;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,6 +25,16 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int N){
+        int count = 0;
+        for (int i = N; i > 0 ;i--){
+              if (i%2!=0 && i%3!=0 && i%5!=0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +45,25 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String input) {
+        StringBuilder result = new StringBuilder();
+        boolean capitaliseNext = false;
 
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '-' || input.charAt(i) == '_') {
+                capitaliseNext = true;
+            }
+            else {
+                if (capitaliseNext) {
+                    result.append(Character.toUpperCase(input.charAt(i)));
+                    capitaliseNext = false;
+                }
+                else {
+                    result.append(input.charAt(i));
+                }
+            }
+        }
+        return result.toString();
+    }
 
 }
